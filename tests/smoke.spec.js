576942688ca21850a7b89fed5975f63bf170b60e
Sophia Dumych <sofiia.dumych.oi.2024@lpnu.ua>
2025-12-26 00:33:58 +0200

    Delete tests/smoke.spec.js

--- tests/smoke.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Home page opens', async ({ page }) => {
-    await page.goto('https://coffee-cart.app/');
-    await expect(page).toHaveTitle(/Coffee/);
-});
