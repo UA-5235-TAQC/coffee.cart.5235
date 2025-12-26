5fca8b564bd5d5ee6fbd2da5969a2e8e2027d14f
EvanShabarovskiy <ivanshabarovskij@gmail.com>
2025-12-26 14:52:51 +0200

    Added class. Removed unnecessary lines. Improved structure

--- page/MenuPage.ts
+++ page/MenuPage.ts
@@ -1,24 +1,16 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
-//import { PromoModal } from "./component/PromoModal";
-//import { CoffeeCartComponent } from "./component/CoffeeCartComponent";
-//import { IngredientComponent } from "./component/IngredientComponent";
+import { CoffeeCartComponent } from "../component/CoffeeCartComponent";
 import { StringUtils } from "../utils/stringUtils";
 import { CoffeeTypes } from "../data/CoffeeTypes";
 
 export  class MenuPage extends BasePage {
-    //protected readonly ingredients: IngredientComponent;
-    //protected readonly promoModal: PromoModal;
-    //protected readonly coffee: CoffeeCartComponent;
-    //protected readonly addToCartModal: AddToCartModal;     
+    protected coffee: CoffeeCartComponent;     
     protected totalBtn: Locator;
 
     constructor(page: Page) {
         super(page);
-        //this.ingredients = new IngredientComponent(page);
-        //this.promoModal = new PromoModal(page);
-        //this.coffee = new CoffeeCartComponent(page);
-        //this.addToCartModal = new AddToCartModal(page);
+        this.coffee = new CoffeeCartComponent(page);
         this.totalBtn = page.getByLabel('Proceed to checkout');
     }
     async navigate(): Promise<void> {
@@ -37,15 +29,15 @@ export  class MenuPage extends BasePage {
     async addRandomCoffeeToCart() {
         const coffeeNames = Object.values(CoffeeTypes);
         const randomName = coffeeNames[Math.floor(Math.random() * coffeeNames.length)];
-        //const coffee = this.getCoffeeItem(randomName); //change on real method
-        //await coffee.click();
+        const coffeeItem = this.coffee.get(randomName); //change on real method
+        await coffeeItem.click();
     }
 
      async showConfirmModal() {
         const coffeeNames = Object.values(CoffeeTypes);
         const randomName = coffeeNames[Math.floor(Math.random() * coffeeNames.length)];
-        //const coffee = this.getCoffeeItem(randomName); change on real method
-        //await coffee.root.click({ button: 'right' });
+        const coffeeItem = this.coffee.get(randomName); //change on real method
+        await coffeeItem.click({ button: 'right' });
     }
 
     async showPromoModal() {
@@ -53,8 +45,10 @@ export  class MenuPage extends BasePage {
 
         for (let i = 0; i < 3; i++) {
             const randomName = coffeeNames[Math.floor(Math.random() * coffeeNames.length)];
-            //const coffee = this.getCoffeeItem(randomName);
-            //await coffee.root.click();
+            const coffeeItem = this.coffee.get(randomName); //change on real method
+            await coffeeItem.click();
         }
     }
+
+    async showCheckout() { await this.totalBtn.hover(); }
 }
