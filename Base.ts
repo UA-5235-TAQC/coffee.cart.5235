f545c1d7ef072eaf4a7c847e31b463bf8a8e8b3a
Maksym Zymyn <maksym.rizdvjanyj@gmail.com>
2025-12-30 12:51:16 +0200

    Remove empty navigate method from Base

--- Base.ts
+++ Base.ts
@@ -3,7 +3,6 @@ import { Page } from "@playwright/test";
 export abstract class Base {
     constructor(protected page: Page) {}
 
-    async navigate(): Promise<void> {}
     abstract isVisible(): Promise<boolean>;
     abstract waitForVisible(): Promise<void>;
     abstract waitForHidden(): Promise<void>;
