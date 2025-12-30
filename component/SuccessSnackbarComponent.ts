dadcebe3c7bd170c5ee25703991eae4d9c898d31
Maksym Zymyn <maksym.rizdvjanyj@gmail.com>
2025-12-30 15:44:47 +0200

    Fix names (#71)
    Co-authored-by: Liubomyr Halamaha <galamagal86@gmail.com>

--- component/SuccessSnackbarComponent.ts
+++ component/SuccessSnackbarComponent.ts
@@ -2,27 +2,27 @@ import { Page, Locator } from "@playwright/test";
 import { Base } from "../Base";
 
 export class SuccessSnackbarComponent extends Base {
-    protected snackbarContainer: Locator;
+    protected root: Locator;
 
     constructor(page: Page) {
         super(page);
-        this.snackbarContainer = this.page.locator('div.snackbar.success');
+        this.root = this.page.locator('div.snackbar.success');
     }
 
     async isVisible(): Promise<boolean> {
-        return this.snackbarContainer.isVisible();
+        return this.root.isVisible();
     }
 
     async waitForVisible(): Promise<void> {
-        await this.snackbarContainer.waitFor({ state: "visible" });
+        await this.root.waitFor({ state: "visible" });
     }
 
     async waitForHidden(): Promise<void> {
-        await this.snackbarContainer.waitFor({ state: "hidden" });
+        await this.root.waitFor({ state: "hidden" });
     }
 
     async getMessage(): Promise<string> {
         await this.waitForVisible();
-        return (await this.snackbarContainer.innerText()).trim();
+        return (await this.root.innerText()).trim();
     }
 }
