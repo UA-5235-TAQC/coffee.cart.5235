865e6c128e88671a693c07af34d5b3e7071ee496
olha-hoshko <olha.hoshko.w@gmail.com>
2025-12-30 13:36:05 +0200

    added basic abstract methods implementation to page classes.

--- page/MenuPage.ts
+++ page/MenuPage.ts
@@ -2,12 +2,18 @@ import { Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export  class MenuPage extends BasePage {
-
-
     constructor(page: Page) {
         super(page);
     }
+
     async navigate(): Promise<void> {
         await this.page.goto("/");
     }
+
+    async isVisible(): Promise<boolean> {
+        return this.page.isVisible("");
+    }
+
+    async waitForVisible(): Promise<void> {}
+    async waitForHidden(): Promise<void> {}
 }
