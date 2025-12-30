51839c3ad3b12acc4e72178f4537e29b7eb2a181
Liubomyr Halamaha <galamagal86@gmail.com>
2025-12-30 14:39:05 +0200

    update

--- component/IngredientComponent.ts
+++ component/IngredientComponent.ts
@@ -1,41 +1,33 @@
 import { Locator } from '@playwright/test';
-import { BasePage } from '../page/BasePage'; 
+import { BasePage } from '../page/BasePage';
+import { IngredientValue } from '../data/IngredientTypes';
 
-export enum Ingredient {
-  Espresso = 'espresso',
-  SteamedMilk = 'steamed milk',
-  MilkFoam = 'milk foam',
-  WhippedCream = 'whipped cream',
-  ChocolateSyrup = 'chocolate syrup',
-  Water = 'water',
-  SteamedCream = 'steamed cream',
-}
 
 export class IngredientComponent extends BasePage {
-  navigate(): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  private readonly rootElement: Locator;
+    navigate(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    private readonly rootElement: Locator;
 
-  constructor(rootElement: Locator) {
-    super(rootElement.page()); 
-    this.rootElement = rootElement;
-  }
+    constructor(rootElement: Locator) {
+        super(rootElement.page());
+        this.rootElement = rootElement;
+    }
 
-  getIngredientByName(name: string): Locator {
-    return this.rootElement.locator('text="${name}"');
-}
-  async getName(): Promise<string> {
-    return (await this.rootElement.innerText()).trim();
-  }
+    getIngredientByName(name: IngredientValue): Locator {
+        return this.rootElement.locator(`text="${name}"`);
+    }
+    async getName(): Promise<string> {
+        return (await this.rootElement.innerText()).trim();
+    }
 
-  async isVisible(): Promise<boolean> {
-    return await this.rootElement.isVisible();
-  }
+    async isVisible(): Promise<boolean> {
+        return await this.rootElement.isVisible();
+    }
 
-  async getColor(): Promise<string> {
-    return await this.rootElement.evaluate((el) => {
-      return window.getComputedStyle(el).color;
-    });
-  }
+    async getColor(): Promise<string> {
+        return await this.rootElement.evaluate((el) => {
+            return window.getComputedStyle(el).color;
+        });
+    }
 }
