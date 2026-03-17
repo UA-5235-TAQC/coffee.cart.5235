deaf493c230204211ee1ca2b8a1fd48d87562bab
Liubomyr Halamaha <galamagal86@gmail.com>
2026-03-17 16:39:55 +0200

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import env from "./config/env";
 
 /**
  * Read environment variables from file.
@@ -31,7 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: process.env.HEADLESS ? process.env.HEADLESS === 'true' : false,
+    headless: env.HEADLESS,
   },
 
   /* Configure projects for major browsers */
